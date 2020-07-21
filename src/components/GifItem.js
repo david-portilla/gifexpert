3caf2b353c12a4f75c9a7447a6bdeea00fb1ee5b
David Portilla <david_portilla@hotmail.com>
2020-07-21 11:06:27 -0500

    update list styles, add getGif component

--- src/components/GifItem.js
+++ src/components/GifItem.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 export const GifItem = ({ id, title, url }) => {
   return (
-    <div>
+    <div className="card">
       <img key={ id } src={ url } alt={ title } />
+      <p> { title }</p>
     </div>
   )
 }
--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,33 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import GifItem from './GifItem'
+import { getImages } from './helpers/getGifs'
 
 export const GifList = ({ category }) => {
 
   const [ images, setImages ] = useState([])
 
   useEffect(() => {
-    getImages()
-  }, [])
-
-  const getImages = async () => {
-    const URL = 'https://api.giphy.com/v1/gifs/search?q=vegeta&limit=10&api_key=oC8I9foQB9Y1uSSK4dEKfF38kcVQFAEa'
-    const res = await fetch(URL)
-    const { data } = await res.json()
-
-    const gifs = data.map(img => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url
-      }
-    })
-    // console.log(gifs)
-    setImages(gifs)
-  }
+    getImages(category)
+      .then(img => setImages(img))
+  }, [ category ])
 
   return (
-    <>
-      <h3 key={ category }>{ category }</h3>
+    <div className="card-list">
+      <h3 className="card-list-title" key={ category }>{ category }</h3>
       {
         images.map(img => (
           <GifItem
@@ -35,6 +21,6 @@ export const GifList = ({ category }) => {
             { ...img } />
         ))
       }
-    </>
+    </div>
   )
 }
