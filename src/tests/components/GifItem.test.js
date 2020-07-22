2f85b6876d4d761470dfe9a5530163f719cb9d14
David Portilla <david_portilla@hotmail.com>
2020-07-22 18:52:57 -0500

    testing props for GifItem

--- src/tests/components/GifItem.test.js
+++ src/tests/components/GifItem.test.js
@@ -6,9 +6,26 @@ describe('Testing <GifItem />', () => {
   const id = 'dadad'
   const title = 'gif title'
   const url = 'www.testingmywebsite.com'
+  const wrapper = shallow(<GifItem id={ id } title={ title } url={ url } />)
 
   test('should show <GifItem />', () => {
-    const wrapper = shallow(<GifItem id={ id } title={ title } url={ url } />)
     expect(wrapper).toMatchSnapshot()
   })
+
+  test('should has a paragraph and use the title props', () => {
+    const p = wrapper.find('p')
+    expect(p.text().trim()).toBe(title)
+  })
+
+  test('should get image from props', () => {
+    const img = wrapper.find('img')
+    expect(img.prop('alt')).toBe(title)
+    expect(img.prop('src')).toBe(url)
+  })
+
+  test('should has classname = {animate__fadeIn}', () => {
+    const div = wrapper.find('div')
+    expect(div.prop('className').includes('animate__fadeIn')).toBe(true)
+  })
+
 })
