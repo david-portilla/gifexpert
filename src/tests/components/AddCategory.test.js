f8eb3a3297963cd85495de1a8d46c0d2501545ce
David Portilla <david_portilla@hotmail.com>
2020-07-25 18:05:16 -0500

    emulate submit and set categories

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import '@testing-library/jest-dom';
+
 const { shallow } = require('enzyme')
 const { AddCategory } = require('../../components/AddCategory')
 
 describe('Test <AddCategory />', () => {
 
-  const setCategories = () => { }
-  const wrapper = shallow(<AddCategory setCategories={ setCategories } />)
+  const setCategories = jest.fn()
+  let wrapper = shallow(<AddCategory setCategories={ setCategories } />)
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+    wrapper = shallow(<AddCategory setCategories={ setCategories } />)
+  })
 
   test('should recive a function', async () => {
     expect(wrapper).toMatchSnapshot()
@@ -15,6 +22,23 @@ describe('Test <AddCategory />', () => {
     const input = wrapper.find('input')
     const value = 'Hello world'
     input.simulate('change', { target: { value: value } })
+  })
+
+  test('disallow submit on empty value ', () => {
+    wrapper.find('form').simulate('submit', { preventDefault () { } })
+    expect(setCategories).not.toHaveBeenCalled()
+  })
+
+  test('should call setCategories and clean text input', () => {
+    const value = 'Hello world'
+    // emulate input change value
+    wrapper.find('input').simulate('change', { target: { value } })
+    // emulate submit form
+    wrapper.find('form').simulate('submit', { preventDefault () { } })
+    // call setCategories
+    expect(setCategories).toHaveBeenCalled()
+    // clean input value
+    expect(wrapper.find('input').prop('value')).toBe('')
   })
 
 })
