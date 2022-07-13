f566874098e6e5cb213095048f6f0ff841107113
David Portilla <david_portilla@hotmail.com>
2022-07-13 12:51:11 -0500

    reset master branch

--- src/tests/helpers/getGifs.test.js
+++
@@ -1,9 +0,0 @@
-import { getImages } from '../../components/helpers/getGifs';
-
-describe('Test getGifs Fetch ', () => {
-  test('should get 10 items', async () => {
-
-    const gifs = await getImages('Vegeta')
-    expect(gifs.length).toBe(10)
-  })
-})
