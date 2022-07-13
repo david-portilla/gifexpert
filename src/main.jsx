6ae9294caec40883609d22e0bb2006b4cedd0934
David Portilla <david_portilla@hotmail.com>
2022-07-13 13:51:11 -0500

    initial styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { GifExpertApp } from "./GifExpertApp";
+import "./styles.css";
+
 const portalDiv = document.getElementById("root");
 
 portalDiv &&
